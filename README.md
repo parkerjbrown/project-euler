a358d4ab68821b8223643f0d2d21fc07c19b6264
Parker Brown <parkerjbrown@smccme.edu>
2016-09-30 23:11:43 -0400

    Added README.md and solution to Problem 1

---
+++ README.md
@@ -0,0 +1,3 @@
+**PROJECT EULER PROBLEMS**
+
+This repository will be continually updated as I attempt the problems found at [Project Euler](http://www.projecteuler.net/).
---
+++ problem1.js
@@ -0,0 +1,16 @@
+/** PROJECT EULER | Problem No. 01
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000. */
+
+var findSum = function(limit) {
+	var sum = 0;
+	for (i = 0; i < limit; i++) {
+		if (i % 3 === 0 || i % 5 === 0) {
+			sum += i;
+		}
+	} return sum;
+}
+
+findSum(1000);
